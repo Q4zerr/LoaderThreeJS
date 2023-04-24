e4dae0cf6387aa29f492605665958d8e3380b891
Q4zerr <avet.tanguy1stmg2@gmail.com>
2023-04-24 08:07:37 +0200

    Fonctionnel

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -15,31 +15,31 @@ camera.position.z = 400;
 
 //===================================================== resize
 window.addEventListener("resize", function() {
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.aspect = window.innerWidth / window.innerHeight;
-camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 });
 
 
 //===================================================== Array of points
 var points = [
-[68.5,185.5],
-[1,262.5],
-[270.9,281.9],
-[345.5,212.8],
-[178,155.7],
-[240.3,72.3],
-[153.4,0.6],
-[52.6,53.3],
-[68.5,185.5]
+    [68.5,185.5],
+    [1,262.5],
+    [270.9,281.9],
+    [345.5,212.8],
+    [178,155.7],
+    [240.3,72.3],
+    [153.4,0.6],
+    [52.6,53.3],
+    [68.5,185.5]
 ];
 
 //===================================================== Convert the array of points into vertices
 for (var i = 0; i < points.length; i++) {
-var x = points[i][0];
-var y = 0;
-var z = points[i][1];
-points[i] = new THREE.Vector3(x, y, z);
+    var x = points[i][0];
+    var y = 0;
+    var z = points[i][1];
+    points[i] = new THREE.Vector3(x, y, z);
 }
 //===================================================== Create a path from the points
 var path = new THREE.CatmullRomCurve3(points);
@@ -51,8 +51,7 @@ var geometry = new THREE.TubeGeometry( path, 300, 4, sides, true );
 //===================================================== Basic material
 var material = new THREE.MeshBasicMaterial({
     side : THREE.BackSide,
-    map: new THREE.TextureLoader().load('img/loader.png')
-    // map: new THREE.TextureLoader().load('https://raw.githubusercontent.com/baronwatts/images/master/map5.png')
+    map: new THREE.TextureLoader().load('https://raw.githubusercontent.com/Q4zerr/LoaderThreeJS/7e5396a444be701e2a646b862d8168df6c063726/assets/img/loader.png')
 });
 material.map.wrapS = THREE.RepeatWrapping;
 material.map.wrapT= THREE.RepeatWrapping;
@@ -71,16 +70,16 @@ scene.add(light);
 var percentage = 0;
 
 function animate() {
-percentage += 0.0005;
-var p1 = path.getPointAt(percentage%1);
-var p2 = path.getPointAt((percentage + 0.03)%1);
-camera.position.set(p1.x,p1.y,p1.z);
-camera.lookAt(p2);
-light.position.set(p2.x, p2.y, p2.z);
+    percentage += 0.0005;
+    var p1 = path.getPointAt(percentage%1);
+    var p2 = path.getPointAt((percentage + 0.03)%1);
+    camera.position.set(p1.x,p1.y,p1.z);
+    camera.lookAt(p2);
+    light.position.set(p2.x, p2.y, p2.z);
 
-//Render the scene
-renderer.render(scene, camera);
-requestAnimationFrame(animate);
+    //Render the scene
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
 }
 
 animate();
